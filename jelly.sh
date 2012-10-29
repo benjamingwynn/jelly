66edd092323e0c8bbc184904172d037cb685ee23
Benjamin <benjamin@gwynn.tk>
2012-10-29 18:56:38 +0000

    Add chmod line to fix permissions after uploading.

--- jelly.sh
+++ jelly.sh
@@ -146,6 +146,7 @@ then
     fi
   touch $date2
   echo "Uploading to Goo.im..."
+  chmod 644 out/target/product/$1/"cm-10-"$date1"-UNOFFICIAL-"$1".zip"
   scp -P 2222 out/target/product/$1/"cm-10-"$date1"-UNOFFICIAL-"$1".zip" benjamingwynn@upload.goo.im:public_html/$1/CyanogenMod10/"CyanogenMod10-benjamingwynn-"$date1".zip"
   echo ". "
   echo ".. "
