c226d5d1edae610d6d23c17b5abb2daa5a9daac0
Benjamin <benjamin@gwynn.tk>
2012-10-29 19:03:39 +0000

    Double check everythings okay permission-wise.

--- jelly.sh
+++ jelly.sh
@@ -148,6 +148,7 @@ then
   echo "Uploading to Goo.im..."
   chmod 644 out/target/product/$1/"cm-10-"$date1"-UNOFFICIAL-"$1".zip"
   scp -P 2222 out/target/product/$1/"cm-10-"$date1"-UNOFFICIAL-"$1".zip" benjamingwynn@upload.goo.im:public_html/$1/CyanogenMod10/"CyanogenMod10-benjamingwynn-"$date1".zip"
+  ssh benjamingwynn@upload.goo.im chmod 644 public_html/$1/CyanogenMod10/"CyanogenMod10-benjamingwynn-"$date1".zip"
   echo ". "
   echo ".. "
   echo "... "
